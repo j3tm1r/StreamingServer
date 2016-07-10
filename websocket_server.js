76aa2ce077c85c414d982d8e5b1ec291a06d0c6c
luigi <tolkkiMetal91@gmail.com>
2016-07-10 21:58:11 +0200

    handled multiple pubs

--- websocket_server.js
+++ websocket_server.js
@@ -22,11 +22,12 @@ const VERBOSE = true
  */
 var configuration = undefined;
 var port = 9090;
-var streamer = undefined
+var streamer = undefined;
 var publisherDeviceName = undefined
 var subscribers = [];
 var browserClients = [];
 var params = [];
+var lowParams = undefined, mediumParams = undefined, highParams = undefined;
 var currentQuality = undefined;
 //var bitrates = [];
 //var currentBitrate = undefined;
@@ -43,6 +44,31 @@ String.format = function() {
         theString = theString.replace(regEx, arguments[i]);
     }
     return theString;
+}
+
+function parseHTTPbody(bodyString){
+	var fields = bodyString.split('&');
+	if (fields === undefined || fields.length == 0){
+		return undefined;
+	}
+	var s = '{';
+	fields.forEach(function(entry, index, array) {
+		var ss = entry.split('=');
+		if (ss === undefined || ss.length != 2){
+			return;
+		}
+		var key = '\"'+ss[0]+'\"';
+		var value = '\"'+ss[1]+'\"';
+		var sep = (index == array.length-1) ? '' : ', ';
+		s += ( key + ':' + value + sep );
+	});
+	s += '}';
+	try{
+		return JSON.parse(s);
+	}
+	catch(Exception){
+		return undefined;
+	}
 }
 
 var httpServerCallback = function(request, response) {
@@ -82,7 +108,47 @@ var httpServerCallback = function(request, response) {
 	}
 	else if (request.method == 'POST'){
 		request.on('data', function(chunk) {
-	      	console.log('Received body data: ' +  chunk.toString());
+			var bodyString = chunk.toString();
+	      	console.log('Received body data: ' +  bodyString);
+			var bodyObj = parseHTTPbody(bodyString);
+			console.log(bodyObj);
+			if (bodyObj === undefined){
+				return;
+			}
+			if (bodyObj.hasOwnProperty('congestion') && bodyObj.hasOwnProperty('bitrate')){
+				console.log('CONGESTION MESSAGE RECEIVED: ' + JSON.stringify(bodyObj));
+				var congestionValue = parseInt(bodyObj.congestion);
+				if (isNaN(congestionValue)){
+					console.log('Can\'t parse congestion value ' + bodyObj.congestion);
+					return;
+				}
+				var bitrateValue = bodyObj.bitrate;
+				var targetParams = undefined;
+				switch (bitrateValue){
+					case 'low':
+					case 'LOW':
+						targetParams = lowParams;
+						break;
+					case 'medium':
+					case 'MEDIUM':
+						targetParams = mediumParams;
+						break;
+					case 'high':
+					case 'HIGH':
+						targetParams = highParams;
+						break;
+					default:
+						console.log('Unknown bitrate value: ' + bitrateValue);
+						break;
+				}
+				if (targetParams !== undefined){
+					console.log('Triggered change to params: ' + JSON.stringify(targetParams));
+					var resetMessage = buildParamsObj(targetParams.width, targetParams.height, targetParams.bitrate);
+					resetMessage['type'] = 'reset';
+					console.log('RESET message: '+resetMessage);
+					forwardToStreamer(JSON.stringify(resetMessage));
+				}
+			}
 	    });
 	    request.on('end', function() {
 			response.writeHead(200, 'OK', {'Content-Type': 'text/html'});
@@ -104,141 +170,170 @@ server.listen(port, function() {
 
 var onTextMessageReceived = function(message, webSocket) {
 	var json;
-	if ((json = parseJSON(message.utf8Data)) != null){
-		if (!json.hasOwnProperty('rule')){
-			console.log('No rule for this JSON message. Will discard...');
-			return;
-		}
-		//I'm the sender
-		if (json.rule === PUBLISHER_RULE){
-			switch (json.type) {
-				case 'hello':
-					streamer = webSocket
-		          	publisherDeviceName = json.device
-		          	params = []
-					//each param is in string format 'wxh b'
-		          	json.qualities.forEach(function(element, index, array){
-						var q = element.split(' ');
-						var size = q[0];
-						var bitrate = q[1];
-		        		q = size.split('x');
-		                if (q.length != 2){
-		            		return;
-		        		}
-		                var param = {
-		                	w: parseInt(q[0]),
-		                    h: parseInt(q[1]),
-							b: parseInt(bitrate)
-		                }
-		                params.push(buildParamsObj(param.w, param.h, param.b));
-		        	})
-					currentQuality = params[json.current];
-		    		//bitrates = json.bitrates;
-					//currentBitrate = json.currentBitrate;
-		    		if (VERBOSE) {
-		            	console.log('Hello from publisher '+json.device+'\n My resolutions: '+json.qualities);
-		            	logAll()
-		          	}
-		          	var notice = getQualitiesNoticePacket();
-		          	forwardToBrowsers(JSON.stringify(notice));
-		          	//forwardToSubscribers(JSON.stringify(qualitiesNotice));
-		          	break;
+	if ((json = parseJSON(message.utf8Data)) == null){
+		console.log('Can\'t parse JSON. Discarding text message');
+		return;
+	}
+	if (!json.hasOwnProperty('rule')){
+		console.log('No rule for this JSON message. Will discard...');
+		return;
+	}
 
-				case 'config':
-					var configArray = json.data;
-					var width = json.width;
-					var height = json.height;
-					var encodeBps = json.encodeBps;
-					var frameRate = json.frameRate;
-					currentQuality = buildParamsObj(width, height, encodeBps);
-          			//currentQuality = width + 'x' + height;
-					//currentBitrate = encodeBps;
-		          	if (VERBOSE) console.log('\nNew quality: '+JSON.stringify(currentQuality));
 
-		          	var notice = getQualitiesNoticePacket();
-		          	forwardToBrowsers(JSON.stringify(notice));
-
-				  	setConfigParams(configArray, width, height, encodeBps, frameRate);
-					var response = getConfigPacket();
-					//if (VERBOSE) console.log('sending config: '+JSON.stringify(response)+' to subscribers ');
-					forwardToSubscribers(JSON.stringify(response));
+	//I'm the sender
+	if (json.rule === PUBLISHER_RULE){
+		switch (json.type) {
+			case 'hello':
+				/*if (streamer !== undefined){
+					if (VERBOSE) console.log('Publisher already defined. Discarding HELLO request');
+					webSocket.close();
 					break;
-
-				case 'stream':
-					var response = message.utf8Data;
-					//console.log('seq num -> '+json.num);
-        	if (VERBOSE) process.stdout.write('.');
-					//forwardToSubscribers(response);
-					break;
-
-				case 'reset':
-					break;
-			}
-
-		}
-
-		//I'm the receiver
-		else if (json.rule === SUBSCRIBER_RULE){
-            switch (json.type) {
-                case 'hello':
-                    if (subscribers.indexOf(webSocket) < 0){
-                        subscribers.push(webSocket);
-                        if (VERBOSE){
-                            console.log('Hello from subscriber: '+webSocket.remoteAddress)
-                            logAll()
-                        }
-                    }
-                    break;
-
-                case 'config':
-                    if (VERBOSE) console.log('config requested by sub '+webSocket.remoteAddress)
-                    var conf = getConfigPacket()
-					if (conf === undefined){
-						//no configuration was set, i.e. no stream sender started streaming
-						if (VERBOSE) console.log('no config available yet')
-						break;
+				}*/
+				streamer = webSocket
+				publisherDeviceName = json.device
+				params = []
+				//each param is in string format 'wxh b'
+				json.qualities.forEach(function(element, index, array){
+					var q = element.split(' ');
+					var size = q[0];
+					var bitrate = q[1];
+					q = size.split('x');
+					if (q.length != 2){
+						return;
 					}
-					//send configuration back to the client who made request
-					if (VERBOSE) console.log('sending config params to sub '+webSocket.remoteAddress)
-                    webSocket.sendUTF(JSON.stringify(conf))
-                    break;
-            }
+					var param = {
+						w: parseInt(q[0]),
+						h: parseInt(q[1]),
+						b: parseInt(bitrate)
+					}
+					var newParam = buildParamsObj(param.w, param.h, param.b);
+					if (newParam.bitrate < 1024){
+						lowParams = newParam;
+					}
+					else if (newParam.bitrate < 2048){
+						mediumParams = newParam;
+					}
+					else if (newParam.bitrate < 4096){
+						highParams = newParam;
+					}
+					params.push(newParam);
+				})
+				currentQuality = params[json.current];
+				//bitrates = json.bitrates;
+				//currentBitrate = json.currentBitrate;
+				if (VERBOSE) {
+					console.log('Hello from publisher '+json.device+'\n My resolutions: '+json.qualities);
+					logAll()
+				}
+				var notice = getQualitiesNoticePacket();
+				forwardToBrowsers(JSON.stringify(notice));
+				//forwardToSubscribers(JSON.stringify(qualitiesNotice));
+				break;
+
+			case 'config':
+				var configArray = json.data;
+				var width = json.width;
+				var height = json.height;
+				var encodeBps = json.encodeBps;
+				var frameRate = json.frameRate;
+				currentQuality = buildParamsObj(width, height, encodeBps);
+				//currentQuality = width + 'x' + height;
+				//currentBitrate = encodeBps;
+				if (VERBOSE) console.log('\nNew quality: '+JSON.stringify(currentQuality));
+
+				var notice = getQualitiesNoticePacket();
+				forwardToBrowsers(JSON.stringify(notice));
+
+				setConfigParams(configArray, width, height, encodeBps, frameRate);
+				var response = getConfigPacket();
+				//if (VERBOSE) console.log('sending config: '+JSON.stringify(response)+' to subscribers ');
+				forwardToSubscribers(JSON.stringify(response));
+				break;
+
+			case 'stream':
+				//var response = message.utf8Data;
+				//console.log('seq num -> '+json.num);
+				//if (VERBOSE) process.stdout.write('.');
+				//forwardToSubscribers(response);
+				break;
+
+			case 'reset':
+				break;
 		}
 
-        //I'm the browser client
-        else if (json.rule === BROWSER_RULE){
-            switch (json.type) {
-                case 'hello':
-                    if (browserClients.indexOf(webSocket) < 0){
-                        browserClients.push(webSocket);
-                        if (VERBOSE) {
-                            console.log('Hello from browser client: '+webSocket.remoteAddress)
-                            logAll()
-                        }
-                    }
-                    //must contain qualities, if they exist
-                    var qualitiesNotice = getQualitiesNoticePacket();
-                    webSocket.sendUTF(JSON.stringify(qualitiesNotice));
-                    break;
+	}
 
-                case 'reset':
-                    if (VERBOSE) console.log('Received RESET from client '+webSocket.remoteAddress)
-                    logAll()
-                    var response = message.utf8Data;
-                    forwardToStreamer(response);
-                    forwardToSubscribers(response);
-                    //webSocket.sendUTF(response)
-                    break;
-            }
-        }
+	//I'm the receiver
+	else if (json.rule === SUBSCRIBER_RULE){
+		var isNew = (subscribers.indexOf(webSocket) < 0);
+		switch (json.type) {
+			case 'hello':
+				if (isNew){
+					subscribers.push(webSocket);
+					if (VERBOSE){
+						console.log('Hello from subscriber: '+webSocket.remoteAddress)
+						logAll()
+					}
+				}
+				break;
 
+			case 'config':
+				if (isNew){
+					if (VERBOSE) console.log('Unknown subscriber');
+					break;
+				}
+				if (VERBOSE) console.log('config requested by sub '+webSocket.remoteAddress)
+				var conf = getConfigPacket()
+				if (conf === undefined){
+					//no configuration was set, i.e. no stream sender started streaming
+					if (VERBOSE) console.log('no config available yet')
+					break;
+				}
+				//send configuration back to the client who made request
+				if (VERBOSE) console.log('sending config params to sub '+webSocket.remoteAddress)
+				webSocket.sendUTF(JSON.stringify(conf))
+				break;
+		}
+	}
+
+	//I'm the browser client
+	else if (json.rule === BROWSER_RULE){
+		var isNew = (browserClients.indexOf(webSocket) < 0);
+		switch (json.type) {
+			case 'hello':
+				if (isNew){
+					browserClients.push(webSocket);
+					if (VERBOSE) {
+						console.log('Hello from browser client: '+webSocket.remoteAddress)
+						logAll()
+					}
+				}
+				//must contain qualities, if they exist
+				var qualitiesNotice = getQualitiesNoticePacket();
+				webSocket.sendUTF(JSON.stringify(qualitiesNotice));
+				break;
+
+			case 'reset':
+				if (isNew){
+					if (VERBOSE) console.log('Unknown web client');
+					break;
+				}
+				if (VERBOSE) console.log('Received RESET from client '+webSocket.remoteAddress)
+				logAll()
+				var response = message.utf8Data;
+				forwardToStreamer(response);
+				forwardToSubscribers(response);
+				//webSocket.sendUTF(response)
+				break;
+		}
 	}
 }
 
 function onBinaryMessageReceived(message, webSocket){
-		for (var i=0; i < subscribers.length; i++) {
-				subscribers[i].sendBytes(message.binaryData);
-		}
+	if (VERBOSE) process.stdout.write('.');
+	for (var i=0; i < subscribers.length; i++) {
+		subscribers[i].sendBytes(message.binaryData);
+	}
 }
 
 
@@ -246,13 +341,24 @@ wsServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
 });
+wsServer.config.maxReceivedMessageSize = 16*1024*1024
+wsServer.config.maxReceivedFrameSize = 512*1024
 
-
+Object.prototype.getName = function() {
+   var funcNameRegex = /function (.{1,})\(/;
+   var results = (funcNameRegex).exec((this).constructor.toString());
+   return (results && results.length > 1) ? results[1] : "";
+};
 
 wsServer.on('request', function(request) {
-	//console.log('REQ from '+request.origin);
-	wsServer.config.maxReceivedMessageSize = 16*1024*1024
-	wsServer.config.maxReceivedFrameSize = 512*1024
+
+	if (!originIsAllowed(request)){
+		if (VERBOSE) console.log('Connection refused');
+		return;
+	}
+
+
+
     var connection = request.accept(null,request.origin);
     console.log((new Date()) + ' Connection accepted.')
     connection.on('message', function(message){
@@ -260,16 +366,14 @@ wsServer.on('request', function(request) {
     		onTextMessageReceived(message, connection);
     	}
     	else if (message.type === 'binary') {
-			/*console.log('Received' + message.binaryData.length + 'bytes');
-			*/
 			onBinaryMessageReceived(message, connection);
 		}
     });
     connection.on('close', function(reasonCode, description) {
-        if (connection === streamer){
+		if (connection === streamer){
             streamer = undefined;
             publisherDeviceName = undefined;
-            params = [];
+            params = []; lowParams = undefined; mediumParams = undefined; highParams = undefined;
             currentQuality = undefined;
             //bitrates = [];
             //currentBitrate = undefined;
@@ -368,8 +472,19 @@ function parseJSON(str){
 }
 
 
-function originIsAllowed(origin) {
+function originIsAllowed(request) {
   // put logic here to detect whether the specified origin is allowed.
+  if (streamer === undefined || streamer.remoteAddress === undefined){
+	  return true;
+  }
+
+  var hds = request.httpRequest.headers;
+  var isPub = (hds.hasOwnProperty('rule') && (hds.rule === PUBLISHER_RULE));
+
+  if (isPub && (streamer.remoteAddress !== request.remoteAddress)){
+	  if (VERBOSE) console.log('Publisher already defined');
+	  return false;
+  }
   return true;
 }
 
@@ -388,4 +503,23 @@ function logAll(){
     s += ']'
     console.log('browser clients: '+s)
 	//console.log('configuration= '+JSON.stringify(configuration))
+}
+
+function isStreamerAllowed(webSocket, messageObj){
+	if (webSocket === undefined) return false;
+	var rule = messageObj.rule;
+	if (streamer === undefined){
+		return rule === PUBLISHER_RULE;
+	}
+	return (rule === PUBLISHER_RULE) && (webSocket === streamer);
+}
+
+function isSubscriberAllowed(webSocket, messageObj){
+	if (webSocket === undefined) return false;
+	return messageObj.rule === SUBSCRIBER_RULE;
+}
+
+function isBrowserAllowed(webSocket, messageObj){
+	if (webSocket === undefined) return false;
+	return messageObj.rule === BROWSER_RULE;
 }
